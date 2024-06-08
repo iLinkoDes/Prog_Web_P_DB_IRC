69994e173c1a93bfeea1745b549a946900f16655
iLinko <iianriverk@gmail.com>
2024-06-07 19:15:06 -0600

    Revert "Creacion de getters y setters"
    This reverts commit 8120c9956499cd52d68e63942d0e5f6d55fcf036.

--- src/java/beans/Escuela.java
+++ src/java/beans/Escuela.java
@@ -4,18 +4,10 @@
  */
 package beans;
 
-import java.io.Serializable;
-
 /**
  *
  * @author iLeLi
  */
-public class Escuela implements java.io.Serializable{
-    private String claveEscuela;
-    private String nombreEscuela;
-    private String domicilio;
-    private int codigoPostal;
-    private String turno;
-    private int idLocalidad;
-            
+public class Escuela {
+    
 }
